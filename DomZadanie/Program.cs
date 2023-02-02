ce2f7be0d28480e4e8a7cf7f4d66340066d105e8
NikitsinIlya <JamsN87@gmail.com>
2023-02-03 02:25:36 +0300

    add gip Goldbah

--- DomZadanie/Program.cs
+++ DomZadanie/Program.cs
@@ -88,7 +88,7 @@ if(zdvig > 0 )
 */
 
 // Гипотеза Гольдбаха )) не решил
-
+/*
 Console.Write("Введите число : ");
 int n = Convert.ToInt32(Console.ReadLine()), n1 = 1, n2 = 0, p1 = 2, count = 0; 
 double sqrtn = Math.Round(Math.Sqrt(n));
@@ -109,4 +109,31 @@ for( int i = 2; i < n/2; i++)
     }
     p1 = 2;
  
+}
+*/
+// решение гипГольдбах
+int n = Convert.ToInt32(Console.ReadLine());
+int countDel = 0, i, j, k, m;
+for (i = 2; i <= n / 2; i++)
+{
+countDel = 0;
+for (j = 2; j <= i / 2; j++)
+{
+if (i % j == 0)
+countDel++;
+}
+if (countDel == 0)
+{
+m = n - i; // 992 - 73 = 919
+for (k = 2; k <= (m + 1) / 2; k++)
+{
+if (m % k == 0)
+countDel++;
+}
+if (countDel == 0)
+{
+Console.WriteLine($"{i} {m}");
+return;
+}
+}
 }
